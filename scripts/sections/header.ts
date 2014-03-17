1ca6e5ee6995828323b785281bdb605978ce32eb
Marcel Ferreira <lfmarcel@gmail.com>
2014-03-17 10:41:56 -0300

    first commit

--- scripts/sections/header.ts
+++ scripts/sections/header.ts
@@ -1,5 +1,34 @@
-# $('./body') {
-#   insert_top("header", class: "_header") {
-#     Move stuff here
-#   }
-# }
+ $('./body') {
+ 	$('./div[@id="Container"]') {
+ 		$('div[@id="AjaxLoading"]') {
+ 			remove()
+ 		}
+ 	}
+   insert_top("header", class: "_header") {
+		move_here('ancestor::body/div[@id="Container"]/div[@id="TopMenu"]', 'bottom')
+		move_here('ancestor::body/div[@id="Container"]/div[@id="Outer"]/div[@id="Header"]', 'top')
+		$('./div[@id="Header"]') {
+			$('./div[@id="Searchform"]') {
+				wrap("div", class: "mw_search_btn sprites-search")
+			}
+		}
+		$('./div[@id="TopMenu"]') {
+			attribute("data-ur-set","toggler")
+			$('./ul') {
+				attribute("data-ur-toggler-component","content")
+			}
+			insert("div","top") {
+				add_class("bot")
+				attribute("data-ur-toggler-component","button")
+			}
+
+		}
+  }
+  $(".//div[@id='Logo']") {
+  # Move top menu into #Logo container
+  move_here("../../../div[@id='TopMenu']", "bottom") {
+    # Remove unwanted top menu items from upper menu
+    remove(".//li[not(contains(@class, 'CartLink') or contains(@class, 'First'))]")
+  }
+}
+}
